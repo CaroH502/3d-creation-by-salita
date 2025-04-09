294de3de23d727335c2553451999c482cfdcd21a
CarolineH <carolinehachebonvin@gmail.com>
2025-04-09 21:13:24 +0200

    add color bleu pétrol and rose pale

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@
   theme: {
     extend: {
       colors: {
-        primary: '#9333EA',    // Exemple : bleu principal
-        secondary: '#9333EA',  // Exemple : violet secondaire
-        accent: '#F59E0B',     // Exemple : orange accent
-        background: '#F3F4F6', // Exemple : fond clair
-        text: '#1F2937',       // Exemple : texte foncé
+        primary: '#00617B',    // Exemple : bleu principal
+        secondary: '#FFF2F4',  // Exemple : violet secondaire
+        //accent: '#F59E0B',     // Exemple : orange accent
+        //background: '#F3F4F6', // Exemple : fond clair
+        //text: '#1F2937',       // Exemple : texte foncé
       }
     },
   },
