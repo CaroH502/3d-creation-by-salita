23fc6deed3f06dae83f7ef662c9d70dd263ff9d8
CarolineH <carolinehachebonvin@gmail.com>
2025-04-09 21:02:18 +0200

    Menu responsif & tailwind custom color ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
  /** @type {import('tailwindcss').Config} */
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9333EA',    // Exemple : bleu principal
+        secondary: '#9333EA',  // Exemple : violet secondaire
+        accent: '#F59E0B',     // Exemple : orange accent
+        background: '#F3F4F6', // Exemple : fond clair
+        text: '#1F2937',       // Exemple : texte foncé
+      }
+    },
   },
   plugins: [],
 }
