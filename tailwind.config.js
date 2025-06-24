730b00fba50ff4a6a87fd89633b9779aa981dc8c
CarolineH <carolinehachebonvin@gmail.com>
2025-06-24 21:32:19 +0200

    version finie desktop, mais avec css et tailwind dans index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-
-    },
-  },
+                extend: {
+                    colors: {
+                        'teal': {
+                            500: '#0D7A8A',
+                            600: '#0A6978',
+                            700: '#085967',
+                        },
+                        'pink': {
+                            100: '#FFF0F0',
+                            200: '#FFE1E1',
+                            300: '#FFD2D2',
+                            400: '#E594A3',
+                            500: '#DA7C8E',
+                        }
+                    },
+                    spacing: {
+                        '14': '3.5rem',   // 56px - title to content spacing
+                        '30': '7.5rem',   // 120px - figma margins
+                        '40': '10rem',    // 160px - section spacing
+                    },
+                    maxWidth: {
+                        'figma': '1200px', // 1440px - (120px * 2)
+                    }
+                }
+            },
   plugins: [],
 }
