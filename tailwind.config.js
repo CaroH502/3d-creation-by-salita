cf4a98c37507c36a1ae3a360e4a8e36acd8a2b82
CarolineH <carolinehachebonvin@gmail.com>
2025-06-24 21:22:22 +0200

    J'arrive pas à sortir ma config tailwind d'index.html, ni le css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,9 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-  content: ["./src/**/*.html", "./src/**/*.js"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'teal': {
-                  500: '#00617B',
-                  600: '#0A6978',
-                  700: '#085967'
-              },
-              'pink': {
-                  100: '#FFF2F4',
-                  200: '#FFE1E1',
-                  300: '#FFD2D2'
-              },
-        primary: '#00617B',    // Exemple : bleu principal
-        secondary: '#FFF2F4',  // Exemple : violet secondaire
-      }
+
     },
   },
   plugins: [],
